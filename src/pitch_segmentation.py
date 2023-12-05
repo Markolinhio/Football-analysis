47e2a3d4ffd9dedfed74cf1d004ef41889030cd0
hoanganhminh01 <hoanganhminh01@gmail.com>
2023-12-05 00:18:22 -0800

    UNet

--- src/pitch_segmentation.py
+++ src/pitch_segmentation.py
@@ -31,30 +31,73 @@ torch.cuda.empty_cache()
 torch.cuda.is_available()
 
 
+class conv_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+        self.relu = nn.ReLU()
+            
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        return x
+
+        
+class encoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv = conv_block(in_c, out_c)
+        self.pool = nn.MaxPool2d((2, 2))
+        
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        p = self.pool(x)
+        return x, p
+    
+    
+class decoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.conv = conv_block(out_c + out_c, out_c)
+        
+    def forward(self, inputs, skip):
+        x = self.up(inputs)
+        x = torch.cat([x, skip], axis=1)
+        x = self.conv(x)
+        return x    
+    
+    
 class PitchSegmentationModel(nn.Module):
     def __init__(self):
         super(PitchSegmentationModel, self).__init__()
 
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(16, 16, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            #nn.MaxPool2d(kernel_size=2, stride=2)
-        )
+        self.e1 = encoder_block(3, 16)
+        self.e2 = encoder_block(16, 32)
+        #self.e3 = encoder_block(32, 64)
+        self.b = conv_block(32, 64)
+        self.d1 = decoder_block(64, 32)
+        self.d2 = decoder_block(32, 16)
+        #self.d3 = decoder_block(32, 16)
+        self.outputs = nn.Conv2d(16, 1, kernel_size=1, padding=0)
 
-        self.decoder = nn.Sequential(
-            nn.Conv2d(16, 16, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(16, 1, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+    def forward(self, inputs):
+        s1, p1 = self.e1(inputs)
+        s2, p2 = self.e2(p1)
+        #s3, p3 = self.e3(p2)
+        b = self.b(p2)
+        d1 = self.d1(b, s2)
+        d2 = self.d2(d1, s1)
+        #d3 = self.d3(d2, s1)
+        outputs = self.outputs(d2)
+        return outputs
     
 
 class PitchDataset(Dataset):
@@ -73,7 +116,7 @@ class PitchDataset(Dataset):
     def __getitem__(self, idx):
         image_info = self.annotations['images'][idx]
         image_name = image_info["file_name"]
-        print(image_name)
+        #print(image_name)
         image_id = image_info["id"]
         annotation_list = [annotation 
                            for annotation in self.annotations['annotations']
