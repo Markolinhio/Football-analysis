7bc6b0dc4fdcd7b1f1c9d584e8628ffc5c1fa7bf
hoanganhminh01 <hoanganhminh01@gmail.com>
2023-12-09 23:43:08 -0800

    Add UNet model

--- src/pitch_segmentation.py
+++ src/pitch_segmentation.py
@@ -81,26 +81,52 @@ class PitchSegmentationModel(nn.Module):
 
         self.e1 = encoder_block(3, 16)
         self.e2 = encoder_block(16, 32)
-        #self.e3 = encoder_block(32, 64)
         self.b = conv_block(32, 64)
         self.d1 = decoder_block(64, 32)
         self.d2 = decoder_block(32, 16)
-        #self.d3 = decoder_block(32, 16)
         self.outputs = nn.Conv2d(16, 1, kernel_size=1, padding=0)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, inputs):
         s1, p1 = self.e1(inputs)
         s2, p2 = self.e2(p1)
-        #s3, p3 = self.e3(p2)
         b = self.b(p2)
         d1 = self.d1(b, s2)
         d2 = self.d2(d1, s1)
-        #d3 = self.d3(d2, s1)
         outputs = self.outputs(d2)
         return self.sigmoid(outputs)
     
 
+class PitchUNet(nn.Module):
+    def __init__(self):
+        super(PitchUNet, self).__init__()
+
+        self.e1 = encoder_block(3, 64)
+        self.e2 = encoder_block(64, 128)
+        self.e3 = encoder_block(128, 256)
+        self.e4 = encoder_block(256, 512)
+        self.b = conv_block(512, 1024)
+        self.d4 = decoder_block(1024, 512)
+        self.d3 = decoder_block(512, 256)
+        self.d2 = decoder_block(256, 128)
+        self.d1 = decoder_block(128, 64)
+        self.outputs = nn.Conv2d(64, 4, kernel_size=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, inputs):
+        s1, p1 = self.e1(inputs)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+        b = self.b(p4)
+        d1 = self.d4(b, s4)
+        d2 = self.d3(d1, s3)
+        d3 = self.d2(d2, s2)
+        d4 = self.d1(d3, s1)
+        outputs = self.outputs(d4)
+        return self.sigmoid(outputs)   
+    
+    
 class PitchDataset(Dataset):
     def __init__(self, image_folder, annotation_file, transform=None):
                         #image_transform=None, mask_transform=None):
